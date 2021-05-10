088c416da76703e6c47d599a0f92105bdffaf780
Eric1891 <79239686+Eric1891@users.noreply.github.com>
2021-05-10 19:40:55 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,46 @@
+
 function setup() {
-  createCanvas(800, 600, WEBGL);//change the createCanvas
+  createCanvas(720, 800, WEBGL);
+  
 }
 
-function draw() {
-  background(230,120,250); //add background
-
-  let locX = mouseX - height / 2;
-  let locY = mouseY - width / 2;
+function draw() { 
+  background(220,120,230); 
+  
+ 
+  let locX = mouseX - height /2;
+  let locY = mouseY - width /2;
 
   ambientLight(60);
   directionalLight(255, 30, 30, 1, 1, 2); //change the directinalLight color
-  pointLight(3, 4, 255, locX, locY, 300); //change the pointLight color
+  pointLight(3, 4, 255, locX, locY, 2); //change the pointLight color
 
   push();
   translate(-width / 5, 3, 5);//change the translate
-  rotateZ(frameCount * 0.05);//change the rotateZ
-  rotateX(frameCount * 0.05);//change  the rotateX 
+  rotateZ(frameCount * mouseX);//change the rotateZ
+  rotateX(frameCount * mouseX);//change  the rotateX 
   specularMaterial(120); //change the specularMaterial color
   box(150, 150, 150); //change box size
   pop();
   
   push();//add new sphare translate
   translate(-width / 10, 8, 4);
-  rotateZ(frameCount * 0.05);
-  rotateX(frameCount * 0.05);
-  ambientMaterial(125);
+  rotateZ(frameCount * mouseY);
+  rotateX(frameCount * mouseX);
+  ambientMaterial(185);
   sphere(60, 120);
   
   push();// add new circle translate
   translate(width / 6, height / 6, 1);
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
+  rotateZ(frameCount * 0.001);
+  rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
   specularMaterial(250);
   torus(100, 40, 64, 64);
   pop();
+  rect(100,200,200,100);
 }
+
+
+   
+
